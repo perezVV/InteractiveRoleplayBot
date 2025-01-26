cc98f37f099e158592db7620d9d2714f0edb084f
Astrea <25420078+AstreaTSS@users.noreply.github.com>
2025-01-26 17:34:35 -0500

    docs: more slight adjustments

--- run.bat
+++ run.bat
@@ -4,10 +4,10 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 REM  uv is used here largely because it makes installation of Python easy.
 REM  At least, it is easier than installing Python manually.
 
-REM  Checks for existence of uv
+REM  Checks for existence of uv.
 TYPE %USERPROFILE%\.local\bin\uv.exe >nul 2>nul
 
-REM  If it doesn't exist, let's install uv
+REM  If it doesn't exist, let's install uv.
 REM  Installation is very simple (just run a command), but we do need to adjust
 REM  the execution policy to install it. We revert it back afterwards.
 REM  Note that the for statement here simply gets the current execution policy.
